cf8c33f1b60a93904a1d04d6268e2a9319e9cd48
AshZi03 <ashrafkazi03@gmail.com>
2024-09-04 07:20:07 +0000

    thrid commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
  
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello, World! i am new');
+  res.send('Hii, World! i am new');
 });
  
 // Define the port number and start the server
