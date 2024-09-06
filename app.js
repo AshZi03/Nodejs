441fdc87b5b7765006a635f6a86e098b12a3433c
AshZi03 <ashrafkazi03@gmail.com>
2024-09-06 15:27:43 +0000

    test 1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
  
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello, World! i am new');
+  res.send('Hello, World! i am new this is automated though pipeline');
 });
  
 // Define the port number and start the server
